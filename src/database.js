8d4f996befbc683a667300d56f412b96776c9cfa
Jean Carlo <jeancarlomo@hotmail.com>
2021-07-02 07:16:23 -0300

    deploy

--- src/database.js
+++ src/database.js
@@ -4,21 +4,13 @@ const {Pool} = pg
 
 
 
+
 const databaseConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.NODE_ENV === "test" ? 'gugas_bakery_test': process.env.DB_DATABASE
-
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 }
-
-// const databaseConfig = {
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// }
 
 
 const connection = new Pool(databaseConfig)
